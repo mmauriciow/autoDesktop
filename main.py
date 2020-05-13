d50121f01e7cc401927b9b157d6187fbd81fe398
¨mmauriciow¨ <m_mendoza@ctinmx.com>
2020-05-13 12:03:48 -0500

    Os

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from actions import *
-
+from sys import platform 
 if __name__ == '__main__':
-    openDrive()
-    sleep(10)
-    login('automationcdemail1@gmail.com', 'Qa123456.')
-    closeDrive()
+    OS = platform
+    print(OS)
+    # openDrive()
+    # sleep(10)
+    # login('automationcdemail1@gmail.com', 'Qa123456.')
+    # closeDrive()
